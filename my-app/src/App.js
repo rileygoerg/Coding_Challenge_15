83f8b12ef225ea90b88cbda3b06a76c32c497996
rileygoerg <rgoerg@usf.edu>
2024-07-05 15:05:23 -0400

    Display educational background in a dedicated section.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,17 @@ function App(){
   const codingSkills = [
     {name:"Javascript", proficiency:'5/10'},
     {name:"CSS", proficiency:"6/10"},
-    {name:"React", proficiency:"2/10"},
+    {name:"React", proficiency:"3/10"},
   ];
   const experience = [
     {role: "Analyst Intern", company:"Grow Financial Credit Union", tenure:"June 2024 - Present"},
     {role: "Bartender", company:"The Melting Pot", tenure:"November 2021 - Present"},
     {role: "Medical Scribe/Assistant", company:"Sinus and Nasal Institute of Florida", tenure:"August 2021 - November 2021"}
+  ];
+  const education = [
+    {institution:"University of South Florida", degree:"B.S. Business Analytics and Information Systems", tenure:"January 2024 - December 2025"},
+    {institution:"State College of Florida", degree:"A.A. Arts", tenure:"August 2016 - May 2018"},
+    {institution:"State College of Florida Collegiate School", degree:"High School Diploma", tenure:"August 2014 - May 2018"}
   ];
   return (
   <div>
@@ -33,6 +38,14 @@ function App(){
       <ol>
         {experience.map(experience=>(
           <li>{experience.role} at {experience.company} | {experience.tenure}</li>
+        ))}
+      </ol>
+    </section>
+    <section>
+      <h3>Education</h3>
+      <ol>
+        {education.map(education =>(
+          <li>{education.institution} | {education.degree} | {education.tenure}</li>
         ))}
       </ol>
     </section>
