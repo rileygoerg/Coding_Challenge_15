467fbae50779da5cc799fb310fc0270faf4b731c
rileygoerg <rgoerg@usf.edu>
2024-07-05 14:49:53 -0400

    Implement dynamic skills section using props.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App(){
-  return (<div>
+  const codingSkills = [
+    {name:"Javascript", proficiency:'5/10'},
+    {name:"CSS", proficiency:"6/10"},
+    {name:"React", proficiency:"2/10"},
+  ];
+  return (
+  <div>
+    <header>
     <h1>Riley Goerg</h1>
     <p>I'm a junior at USF majoring in Business Analytics and Information Systems.</p>
     <p>rgoerg@gmail.com</p>
     <p>linkedin.com/in/riley-goerg</p>
+    </header>
+    <section>
+      <h3>Skills</h3>
+      <ol>
+        {codingSkills.map(skill =>(
+          <li>{skill.name} - {skill.proficiency}</li>
+        ))}
+      </ol>
+    </section>
+
   </div>
   );
 }
