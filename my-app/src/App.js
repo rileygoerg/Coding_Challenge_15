8032242ce9e4e3152a1b0c3f7dea8a45715a36b4
rileygoerg <rgoerg@usf.edu>
2024-07-05 14:40:24 -0400

    Add personal information section with styled components.

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App(){
+  return (<div>
+    <h1>Riley Goerg</h1>
+    <p>I'm a junior at USF majoring in Business Analytics and Information Systems.</p>
+    <p>rgoerg@gmail.com</p>
+    <p>linkedin.com/in/riley-goerg</p>
+  </div>
+  );
+}
+export default App;
